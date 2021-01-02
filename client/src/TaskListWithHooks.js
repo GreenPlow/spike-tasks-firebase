68c188e076696fc60c353696993cb89446b83f5e
nathan leezer <62969830+GreenPlow@users.noreply.github.com>
2021-01-02 15:34:42 -0600

    implement first custom hook

--- client/src/TaskListWithHooks.js
+++ client/src/TaskListWithHooks.js
@@ -5,126 +5,50 @@ import { Card, Header, Form, Input, Icon } from "semantic-ui-react";
 
 let endpoint = "http://localhost:8000";
 
-const testItems = [
-  {
-    "_id": "5fbf265dbaf57a1bd9efbed1",
-    "status": true,
-    "task": "neat"
-  },
-  {
-    "_id": "5fbf2667baf57a1bd9efbed2",
-    "status": false,
-    "task": "other thing"
-  },
-  {
-    "_id": "5febf5a8530b93c97a3b6f25",
-    "status": false,
-    "task": "does this still work"
-  }
-]
+export default function TaskList () {
+  const [newTask, setNewTask] = useState('');
+  const tasks = useTaskList();
 
-// function getAllTasks() {
-//   axios.get(endpoint + "/api/task").then(res => {
-//     console.log(res);
-//     if (res.data) {
-//         return res.data.map(item => {
-//           console.log('call component')
-//       })
-//     }
-//     else {
-//       return []
-//     }
-//   })
-// }
-
-// getAllTasks = () => {
-//   axios.get(endpoint + "/api/task").then(res => {
-//     console.log(res);
-//     if (res.data) {
-//       this.setState({
-//         items: res.data.map(item => {
-//           console.log('call component')
-//         })
-//       })
-//     }
-//     else {
-//     this.setState({
-//       items: []
-//     });
-// }
-
-
-function Row(props) {
-  let color = "yellow";
-
-  if (props.status) {
-    color = "green";
-  }
-
-  return (
-    <Card key={props._id} color={color} fluid>
-      <Card.Content>
-        <Card.Header textAlign="left">
-          <div style={{ wordWrap: "break-word" }}>{props.task}</div>
-        </Card.Header>
-        <Card.Meta textAlign="right">
-          <Icon
-            name="check circle"
-            color="green"
-          />
-          <span style={{ paddingRight: 10 }}>Done</span>
-          <Icon
-            name="undo"
-            color="yellow"
-          />
-          <span style={{ paddingRight: 10 }}>Undo</span>
-          <Icon
-            name="delete"
-            color="red"
-          />
-          <span style={{ paddingRight: 10 }}>Delete</span>
-        </Card.Meta>
-      </Card.Content>
-    </Card>
-  );
-}
-
-function MainApp() {
-  // const [tasks, setItems] = useState(testItems);
-  const [newTask, setTask] = useState('');
-  const [tasks, setTasks] = useState([])
-
-  // useEffect( () => {
-  //   axios.get(endpoint + "/api/task")
-  //   .then(res => { 
-  //     console.log(res)
-  //     if (res.data) {
-  //       setTasks(res.data)
-  //     }
-  //   })
-  // }, [tasks])
-
-  useEffect( async () => {
-    const res = await axios.get(endpoint + "/api/task")
-    console.log(res)
-    if (res.data) {
-      setTasks(res.data)
-    }
-  }, [newTask])
 
   function handleNewTask(e) {
-    setTask(e.target.value);
+    setNewTask(e.target.value);
     console.log('here')
   }
 
-  // useEffect(() => {
-  //   axios.get(endpoint + "/api/task").then(res => {
-  //     console.log(res)
-  //     if (res.data) {
-  //       setItems()
-  //     }
-  //   })
-  // })
+  function Row(props) {
+    let color = "yellow";
+  
+    if (props.status) {
+      color = "green";
+    }
+  
+    return (
+      <Card key={props._id} color={color} fluid>
+        <Card.Content>
+          <Card.Header textAlign="left">
+            <div style={{ wordWrap: "break-word" }}>{props.task}</div>
+          </Card.Header>
+          <Card.Meta textAlign="right">
+            <Icon
+              name="check circle"
+              color="green"
+            />
+            <span style={{ paddingRight: 10 }}>Done</span>
+            <Icon
+              name="undo"
+              color="yellow"
+            />
+            <span style={{ paddingRight: 10 }}>Undo</span>
+            <Icon
+              name="delete"
+              color="red"
+            />
+            <span style={{ paddingRight: 10 }}>Delete</span>
+          </Card.Meta>
+        </Card.Content>
+      </Card>
+    );
+  }
 
   return (
     <div>
@@ -146,13 +70,20 @@ function MainApp() {
         </Form>
       </div>
       <div className="row">
-        <Card.Group>{tasks.map(item => <Row task={item.task} />)}</Card.Group>
+        <Card.Group>{tasks.map(item => <Row key={item._id} task={item.task} />)}</Card.Group>
       </div>
     </div>
   );
-  
+
+  function useTaskList () {
+    const [tasks, setTasks] = useState([])
+    useEffect( async () => {
+      const res = await axios.get(endpoint + "/api/task")
+      console.log(res)
+      if (res.data) {
+        setTasks(res.data)
+      }
+    }, [newTask])
+    return tasks
+  }
 }
-
-export default MainApp;
-
-
