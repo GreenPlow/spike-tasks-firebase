21d022f29679f41a2fe79f640824aa3acd6a7e06
nathan leezer <62969830+GreenPlow@users.noreply.github.com>
2021-01-14 14:38:47 -0500

    remove dynamic solution for updating time

--- client/src/Time.js
+++ client/src/Time.js
@@ -1,13 +1,8 @@
 
 import React, { useState, useEffect } from 'react';
-// import React, { useState, useEffect, useRef } from 'react';
 
 function Time() {
     let [time, setTime] = useState(new Date().toLocaleTimeString());
-
-    // useInterval(() => {
-    //   setTime(new Date().toLocaleTimeString());  
-    // }, 1000);
 
     useEffect(() => {
       let id = setInterval(() => {
@@ -24,28 +19,3 @@ function Time() {
   }
 
   export default Time
-
-  // function useInterval(callback, delay) {
-  //   const savedCallback = useRef();
-  
-  //   // Remember the latest callback.
-  //   useEffect(() => {
-  //     savedCallback.current = callback;
-  //   }, [callback]);
-  
-  //   // Set up the interval.
-  //   useEffect(() => {
-  //     function tick() {
-  //       savedCallback.current();
-  //     }
-  //     if (delay !== null) {
-  //       let id = setInterval(tick, delay);
-  //       // Need to cleanup memory leak when done
-  //       return function cleanup() {
-  //         clearInterval(id)
-  //       };
-  //     }
-  //   }, [delay]);
-  // }
-
-
