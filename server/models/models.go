23a3b4ea86caeb83d50629ddf62a925ed61b13fd
nathan leezer <62969830+GreenPlow@users.noreply.github.com>
2021-01-19 18:32:05 -0500

    remove new field, not part of this PR

--- server/models/models.go
+++ server/models/models.go
@@ -7,7 +7,7 @@ type TaskList struct {
 	ID       primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
 	Task     string             `json:"task,omitempty"`
 	Status   bool               `json:"status,omitempty"`
-	DateTime primitive.DateTime `json:"date,omitempty"`
 }
 
 // needs to be interger in the front end that is a unixtimestamp
+// 	DateTime primitive.DateTime `json:"date,omitempty"`
