f7c505b7f076ac1e6d3c673a05efc069f59ae650
nathan leezer <62969830+GreenPlow@users.noreply.github.com>
2021-01-19 10:34:43 -0500

    add DateTime field to mongo

--- server/models/models.go
+++ server/models/models.go
@@ -4,8 +4,10 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 // TaskList is something that needs to be defined later as it is required by the linter. //TODO
 type TaskList struct {
-	ID primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
-
-	Task   string `json:"task,omitempty"`
-	Status bool   `json:"status,omitempty"`
+	ID       primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Task     string             `json:"task,omitempty"`
+	Status   bool               `json:"status,omitempty"`
+	DateTime primitive.DateTime `json:"date,omitempty"`
 }
+
+// needs to be interger in the front end that is a unixtimestamp
