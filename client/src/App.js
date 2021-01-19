5c506f62da419837b396a6c1add1fb8aa72e3ce3
nathan leezer <62969830+GreenPlow@users.noreply.github.com>
2021-01-19 09:59:18 -0500

    remove HTML5 picker

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,31 @@
-/* eslint-disable */
 import 'react-dates/initialize';
 import 'react-dates/lib/css/_datepicker.css'
 import React, { useState } from 'react';
-import "./App.css";
-
-// import the Container Component from the semantic-ui-react
-import { Container, Header, Form } from "semantic-ui-react";
-import { SingleDatePicker} from 'react-dates';
+import { Container, Header } from "semantic-ui-react";
+import { SingleDatePicker } from 'react-dates';
 import 'react-dates/lib/css/_datepicker.css';
+import moment from 'moment';
 
 import TaskList from "./components/TaskListWithHooks";
 import Time from "./components/Time";
-import moment from 'moment';
+import "./App.css";
 
 function App() {
   const [date, setDate] = useState(moment());
-
   const [isFocused, setFocused] = useState(false);
+  
   return (
     <div>
       <Container>
         <Header className="header" as="h2" textAlign="center">
           <Time />
+          {/* TODO REVIEW THIS LIB FOR A BETTER ONE */}
           <SingleDatePicker
-            date={date} // momentPropTypes.momentObj or null
+            date={date} // momentPropTypes.momentObj or null 
             onDateChange={date => setDate(date)} // PropTypes.func.isRequired
             focused={isFocused} // PropTypes.bool
             onFocusChange={({ focused }) => setFocused(focused)} // PropTypes.func.isRequired
-            id="your_unique_id" // PropTypes.string.isRequired,
+            id="your_unique_id" // PropTypes.string.isRequired //why is this required and what should it be?
           />
         </Header>
         <TaskList />
@@ -36,11 +34,3 @@ function App() {
   );
 }
 export default App;
-
-function DatePicker() {
-  return (
-    <Form onSubmit>
-        <input type="date" placeholder="yyyy-mm-dd" display="flex"></input>
-    </Form>
-  )
-}
