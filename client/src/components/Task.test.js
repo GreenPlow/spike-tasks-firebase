eeaf70766a74fb7db73d27ae31b072e111616762
nathan leezer <62969830+GreenPlow@users.noreply.github.com>
2021-01-26 10:18:11 -0500

    cleanup first two tests

--- client/src/components/Task.test.js
+++ client/src/components/Task.test.js
@@ -1,24 +1,10 @@
-// import React from 'react';
-// import { render, screen } from '@testing-library/react';
-// import App from './App';
-
-// it('renders welcome message', () => {
-//   render(<App />);
-//   expect(screen.getByText('Learn React')).toBeInTheDocument();
-// });
 
 import React from 'react';
-import ReactDOM from 'react-dom'
-
 import {render, cleanup, screen, fireEvent} from '@testing-library/react'
-import { Card, Icon } from "semantic-ui-react";
-
 import Task from "./Task";
 
 import {
-  deleteTask,
-  completeTask,
-  undoTask
+  completeTask
 } from "../taskActions"
 
 jest.mock('../taskActions');
@@ -33,27 +19,17 @@ const item = {
 
 describe('my tests', () => {
 
-  it("renders with or without a name", () => {
+  it("renders a task with provided text from props", () => {
 
     // Arrange
-  
     render(<Task item={item}/>);
     // returns a container, a base element, debug
-    // container.firstChild.firstChildren
-
+    // container.firstChild.firstChild
 
     // Act
 
     // Assert
     expect(screen.getByTestId('hh')).toHaveTextContent('test-task-value')
-
-    // expect(screen.getByText('Learn React')).toBeInTheDocument();
-
-
-    // expect(getByText(/Initial/i).textContent).toBe("Initial State")
-    // fireEvent.click(getByText("State Change Button"))
-    // expect(getByText(/Initial/i).textContent).toBe("Initial State Changed")
-    
   });
 
   it ("should render the done button", async () => {
@@ -88,14 +64,15 @@ describe('my tests', () => {
 })
 
 
-/* The way our end user will use this app will be to: see some text on the UI, 
+/* USER ACTION EXAMPLE
+The way our end user will use this app will be to: see some text on the UI, 
 see the text in the button, then click on it, finally see some new text on UI.
 */
 
+/*  ReactDOM.render()
+    Takes the element, a container, and a callback
+*/
 
-    /*  ReactDOM.render()
-        Takes the element, a container, and a callback
-    */
 // act(() => { render(element, container); });
 // // https://reactjs.org/docs/test-utils.html
 // const stuff = findRenderedComponentWithType(container, Card)
