c2096c4e567f5dd9fd8729f3bcd0ea0fbec4888e
nathan leezer <62969830+GreenPlow@users.noreply.github.com>
2021-01-29 16:08:11 -0500

    format Time component

--- client/src/components/Time.js
+++ client/src/components/Time.js
@@ -1,21 +1,22 @@
-
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 function Time() {
-    let [time, setTime] = useState(new Date());
+  let [time, setTime] = useState(new Date());
 
-    useEffect(() => {
-      let id = setInterval(() => {
-        setTime(new Date())
-      }, 1000);
-      return function cleanup() {
-        clearInterval(id)
-      }
-    })
+  useEffect(() => {
+    let id = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+    return function cleanup() {
+      clearInterval(id);
+    };
+  });
 
-    return (
-      <div>{time.toLocaleTimeString('default', {month: 'long', day: '2-digit'})}</div>
-    )
-  }
+  return (
+    <div>
+      {time.toLocaleTimeString("default", { month: "long", day: "2-digit" })}
+    </div>
+  );
+}
 
-  export default Time
+export default Time;
