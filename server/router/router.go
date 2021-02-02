12dc4c99f5a470ad5337d7be6dc33c3de6e34754
nathan leezer <62969830+GreenPlow@users.noreply.github.com>
2021-02-02 16:49:34 -0500

    break apart pre-flight from the api invocation

--- server/router/router.go
+++ server/router/router.go
@@ -9,7 +9,6 @@ import (
 func Router() *mux.Router {
 	router := mux.NewRouter()
 
-	// TODO write a better handler to work with preflight
 	// pre-flight is invoking the real handler
 
 	router.HandleFunc("/api/task", handlers.GetAllTask).Methods("GET", "OPTIONS")
@@ -17,7 +16,11 @@ func Router() *mux.Router {
 	router.HandleFunc("/api/completeTask/{id}", handlers.CompleteTask).Methods("PUT", "OPTIONS")
 	router.HandleFunc("/api/undoTask/{id}", handlers.UndoTask).Methods("PUT", "OPTIONS")
 	// TODO fix update
-	// router.HandleFunc("/api/updateTask/{id}", handlers.UpdateTask).Methods("PUT", "OPTIONS")
+	router.HandleFunc("/api/updateTask/{id}", handlers.UpdateTaskOptions).Methods("OPTIONS")
+	router.HandleFunc("/api/updateTask/{id}", handlers.UpdateTask).Methods("PUT")
+
+
+	// TODO write a better handler to work with preflight
 	router.HandleFunc("/api/deleteTask/{id}", handlers.DeleteTaskOptions).Methods("OPTIONS")
 	router.HandleFunc("/api/deleteTask/{id}", handlers.DeleteTask).Methods("DELETE")
 
