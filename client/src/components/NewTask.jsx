71286c6805ec331d0ff51455faa35404b414488d
nathan leezer <62969830+GreenPlow@users.noreply.github.com>
2021-02-04 14:32:31 -0500

    add radio buttons with shared state

--- client/src/components/NewTask.jsx
+++ client/src/components/NewTask.jsx
@@ -4,10 +4,12 @@ import React, { useState } from "react";
 
 import { createNewTask } from "../api/taskActions";
 
-import { Form, Input } from "semantic-ui-react";
+import { Form, Input, Radio } from "semantic-ui-react";
+
 
 function NewTask(props) {
   const [newTask, setNewTask] = useState("");
+  const [newTaskSize, setSize] = useState("");
 
   function handleNewTask(e) {
     setNewTask(e.target.value);
@@ -32,6 +34,39 @@ function NewTask(props) {
           onChange={handleNewTask}
         />
       </Form>
+      <Form.Field>
+        <Radio
+          label="small"
+          name="radioGroup"
+          value="small"
+          checked={newTaskSize === "small"}
+          onChange={() => {
+            setSize("small");
+          }}
+        />
+      </Form.Field>
+      <Form.Field>
+        <Radio
+          label="medium"
+          name="radioGroup"
+          value="medium"
+          checked={newTaskSize === "medium"}
+          onChange={() => {
+            setSize("medium");
+          }}
+        />
+      </Form.Field>
+      <Form.Field>
+        <Radio
+          label="large"
+          name="radioGroup"
+          value="large"
+          checked={newTaskSize === "large"}
+          onChange={() => {
+            setSize("large");
+          }}
+        />
+      </Form.Field>
     </div>
   );
 }
