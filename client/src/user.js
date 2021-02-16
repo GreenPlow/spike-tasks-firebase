385c64861ef229004b42cc2fd3cee31c28ee4d85
nathan leezer <62969830+GreenPlow@users.noreply.github.com>
2021-02-16 11:23:20 -0500

    add user.js to set local storage

---
+++ client/src/user.js
@@ -0,0 +1,15 @@
+let user = localStorage.getItem("user") || "horse";
+
+function set(userName) {
+  user = userName;
+  localStorage.setItem("user", user);
+}
+
+function get() {
+  if (!user) {
+    throw new Error("User not logged in!");
+  }
+  return user;
+}
+
+export { set, get };
