b7b8b623f50395ece8274e839751e8526cffa2b2
nathan leezer <62969830+GreenPlow@users.noreply.github.com>
2021-02-23 14:27:37 -0500

    add comments for RESTful api

--- server/router/router.go
+++ server/router/router.go
@@ -26,6 +26,16 @@ func Router() *chi.Mux {
 	// r.Get("/api/task", handlers.GetAllTask)
 	r.Get("/api/task", handlers.GetTasksByDate)
 	r.Post("/api/task", handlers.CreateTask)
+	// r.Delete("/api/task", handlers.CreateTask) // delete everything
+	// r.Put("/api/task/{id}", handlers.CreateTask) // everything
+	// r.Patch("/api/task/{id}", handlers.CreateTask) // only changes
+	// r.Delete("/api/task/{id}", handlers.CreateTask) // delete
+
+	// Convenience Method
+	// r.Post("/api/task/{id}/complete", handlers.CreateTask) // complete
+	// r.Post("/api/task/{id}/undo", handlers.CreateTask) // undo
+
+	// Will eventually remove these endpoints below
 	r.Put("/api/completeTask/{id}", handlers.CompleteTask)
 	r.Put("/api/undoTask/{id}", handlers.UndoTask)
 	r.Put("/api/updateTask/{id}", handlers.UpdateTask)
