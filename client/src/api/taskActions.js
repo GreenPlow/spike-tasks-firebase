765202545fda09032433aa41d3daf7d4b8856799
nathan leezer <62969830+GreenPlow@users.noreply.github.com>
2021-03-09 00:16:08 -0500

    refactor interceptor for Axios

--- client/src/api/taskActions.js
+++ client/src/api/taskActions.js
@@ -8,23 +8,8 @@ const endpoint = "http://localhost:8000";
 
 axios.interceptors.request.use(function (config) {
   config.headers["X-USERNAME"] = get();
-  console.log(config);
   return config;
 });
-
-axios.interceptors.response.use(
-  function (response) {
-    // Any status code that lie within the range of 2xx cause this function to trigger
-    // Do something with response data
-    return response;
-  },
-  function (error) {
-    // Any status codes that falls outside the range of 2xx cause this function to trigger
-    // Do something with response error
-    handleAxiosError(error);
-    return Promise.reject(error);
-  }
-);
 
 async function getLatestTasksFromServer(date) {
   console.log(get());
@@ -57,7 +42,7 @@ async function createNewTask(task, taskSize, date) {
 async function patchTask({ _id, property }) {
   const body = property;
   const url = endpoint + "/api/task/" + _id;
-  console.log("body", body)
+  console.log("body", body);
   await axios.patch(url, body, {
     headers: { "Content-Type": "application/x-www-form-urlencoded" },
   });
