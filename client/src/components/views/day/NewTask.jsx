597111c74c3506e55879ee845195e6b6478ef9d0
nathan leezer <62969830+GreenPlow@users.noreply.github.com>
2021-03-12 16:32:50 -0500

    remove commented code

--- client/src/components/views/day/NewTask.jsx
+++ client/src/components/views/day/NewTask.jsx
@@ -52,7 +52,6 @@ function NewTask({ onCreateFinish, dateObj }) {
 
   async function onSubmit(size) {
     document.activeElement.blur();
-    console.log(size);
 
     try {
       await createNewTask(newTask, size, dateObj.toISOString());
@@ -102,7 +101,6 @@ function NewTask({ onCreateFinish, dateObj }) {
               value=""
               onChange={(value) => {
                 // setNewTaskSize(value);
-                // console.log("value on 113", value);
                 onSubmit(value);
               }}
             >
@@ -129,14 +127,3 @@ NewTask.propTypes = {
 };
 
 export default NewTask;
-
-// <TaskSizeSelector
-// sizeOptions={["small", "medium", "large"]}
-// onSizeChange={(value) => {
-//   console.log("click");
-//   setNewTaskSize(value);
-//   console.log(newTaskSize)
-//   onSubmit()
-// }}
-// selectedSize={newTaskSize}
-// />
