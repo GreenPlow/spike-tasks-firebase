6c577abcfbebdd912f17851e72a9b0979322c9a2
nathan leezer <62969830+GreenPlow@users.noreply.github.com>
2021-04-06 00:15:12 -0400

    refactor firebase singleton

--- client/src/app/config/fire.js
+++ client/src/app/config/fire.js
@@ -4,30 +4,23 @@ import "firebase/auth";
 import "firebase/firestore";
 
 var app;
-var appAuth;
-var appDb;
-var firestore;
 
-function init() {
-  // For Firebase JavaScript SDK v7.20.0 and later, `measurementId` is an optional field
-  const config = {
-    apiKey: "AIzaSyAoubU_6fV8SwNKGRjKxgQas9mXG7OmfLE",
-    authDomain: "spike-task-firebase.firebaseapp.com",
-    projectId: "spike-task-firebase",
-    storageBucket: "spike-task-firebase.appspot.com",
-    messagingSenderId: "80902187036",
-    appId: "1:80902187036:web:229a4feebedf1b9e8f97d9",
-    measurementId: "G-M7VKFY9CV1",
-  };
+// For Firebase JavaScript SDK v7.20.0 and later, `measurementId` is an optional field
+const config = {
+  apiKey: "AIzaSyAoubU_6fV8SwNKGRjKxgQas9mXG7OmfLE",
+  authDomain: "spike-task-firebase.firebaseapp.com",
+  projectId: "spike-task-firebase",
+  storageBucket: "spike-task-firebase.appspot.com",
+  messagingSenderId: "80902187036",
+  appId: "1:80902187036:web:229a4feebedf1b9e8f97d9",
+  measurementId: "G-M7VKFY9CV1",
+};
 
-  // TODO Should these sdk calls be handled for errors? They occur before react is rendered.
+// TODO Should these sdk calls be handled for errors? They occur before react is rendered.
 
-  app = firebase.initializeApp(config);
-  appAuth = firebase.auth(app);
-  appDb = firebase.firestore(app);
-  firestore = firebase.firestore;
-  // TODO remove this console.log before publishing. Here for debugging.
-  console.log("firebase init finished");
-}
+app = firebase.initializeApp(config);
 
-export { init, app, appDb, firestore, appAuth };
+// Initilize firestore
+firebase.firestore(app);
+
+export { firebase };
