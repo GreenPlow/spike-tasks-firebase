168584fa7ad541f132272aae500b1eb00b0f05aa
nathan leezer <62969830+GreenPlow@users.noreply.github.com>
2021-04-06 00:20:50 -0400

    replace createNewTask and getLatestTasksFromServer with Firebase

--- client/src/app/api/taskActions.js
+++ client/src/app/api/taskActions.js
@@ -2,12 +2,14 @@ import axios from "axios";
 import React from "react";
 
 import { setAlert } from "../errorMessage";
-import { appAuth as auth, appDb, firestore } from "../config/fire";
+import { firebase } from "../config/fire";
 
 // add the axios interceptors here to do the banners and logging, able to delete the try catches
 // replace localhost with ip address to access app from a local network
 const endpoint = "http://localhost:8000";
 
+const Timestamp = firebase.firestore.Timestamp;
+const FieldValue = firebase.firestore.FieldValue;
 // async function getLatestTasksFromServer(date) {
 //   console.log(get());
 //   const url =
@@ -25,109 +27,104 @@ const endpoint = "http://localhost:8000";
 
 async function getLatestTasksFromServer(date) {
   console.log(date);
-  try {
-    // let tasksRef = appDb.collection(`users/${auth.currentUser.uid}/tasklist`);
-    // let results = await tasksRef
-    //   .where("date", ">=", firestore.Timestamp.fromDate(date.toDate()))
-    //   .get();
-    // .then((querySnapshot) => {
-    //   querySnapshot.forEach((doc) => {
-    //     // doc.data() is never undefined for query doc snapshots
-    //     console.log(doc.id, " => ", doc.data());
-    //   });
-    // });
+  //   let tasksRef = appDb.collection(`users/${auth.currentUser.uid}/tasklist`);
+  //   let results = await tasksRef
+  //     .where("date", ">=", firestore.Timestamp.fromDate(date.toDate()))
+  //     .get();
+  //   .then((querySnapshot) => {
+  //     querySnapshot.forEach((doc) => {
+  //       // doc.data() is never undefined for query doc snapshots
+  //       console.log(doc.id, " => ", doc.data());
+  //     });
+  //   });
 
-    // citiesRef.doc("SF").set({
-    //   name: "San Francisco",
-    //   state: "CA",
-    //   country: "USA",
-    //   capital: false,
-    //   population: 860000,
-    //   regions: ["west_coast", "norcal"],
-    // });
-    // citiesRef.doc("LA").set({
-    //   name: "Los Angeles",
-    //   state: "CA",
-    //   country: "USA",
-    //   capital: false,
-    //   population: 3900000,
-    //   regions: ["west_coast", "socal"],
-    // });
-    // citiesRef.doc("DC").set({
-    //   name: "Washington, D.C.",
-    //   state: null,
-    //   country: "USA",
-    //   capital: true,
-    //   population: 680000,
-    //   regions: ["east_coast"],
-    // });
-    // citiesRef.doc("TOK").set({
-    //   name: "Tokyo",
-    //   state: null,
-    //   country: "Japan",
-    //   capital: true,
-    //   population: 9000000,
-    //   regions: ["kanto", "honshu"],
-    // });
-    // citiesRef.doc("BJ").set({
-    //   name: "Beijing",
-    //   state: null,
-    //   country: "China",
-    //   capital: true,
-    //   population: 21500000,
-    //   regions: ["jingjinji", "hebei"],
-    // });
-    var citiesRef = appDb.collection(`users/${auth.currentUser.uid}/cities`);
+  // citiesRef.doc("SF").set({
+  //   name: "San Francisco",
+  //   state: "CA",
+  //   country: "USA",
+  //   capital: false,
+  //   population: 860000,
+  //   regions: ["west_coast", "norcal"],
+  // });
+  // citiesRef.doc("LA").set({
+  //   name: "Los Angeles",
+  //   state: "CA",
+  //   country: "USA",
+  //   capital: false,
+  //   population: 3900000,
+  //   regions: ["west_coast", "socal"],
+  // });
+  // citiesRef.doc("DC").set({
+  //   name: "Washington, D.C.",
+  //   state: null,
+  //   country: "USA",
+  //   capital: true,
+  //   population: 680000,
+  //   regions: ["east_coast"],
+  // });
+  // citiesRef.doc("TOK").set({
+  //   name: "Tokyo",
+  //   state: null,
+  //   country: "Japan",
+  //   capital: true,
+  //   population: 9000000,
+  //   regions: ["kanto", "honshu"],
+  // });
+  // citiesRef.doc("BJ").set({
+  //   name: "Beijing",
+  //   state: null,
+  //   country: "China",
+  //   capital: true,
+  //   population: 21500000,
+  //   regions: ["jingjinji", "hebei"],
+  // });
+  const getUser = firebase.auth().currentUser.uid;
 
-    citiesRef
-      .where("capital", "==", true)
-      .get()
-      .then((doc) => {
-        if (doc.exists) {
-          console.log("Document data:", doc.data());
-        } else {
-          // doc.data() will be undefined in this case
-          console.log("No such document!");
-        }
-      })
-      .catch((error) => {
-        console.log("Error getting document:", error);
-      });
+  var citiesRef = firebase.firestore().collection(`users/${getUser}/tasklist`);
 
-    // var docRef = await appDb.collection(`users/${auth.currentUser.uid}/cities`);
-    // citiesRef
-    //   .where("state", "==", "CA")
-    //   .get()
-    //   .then((doc) => {
-    //     if (doc.exists) {
-    //       console.log("Document data:", doc.data());
-    //     } else {
-    //       // doc.data() will be undefined in this case
-    //       console.log("No such document!");
-    //     }
-    //   })
-    //   .catch((error) => {
-    //     console.log("Error getting document:", error);
-    //   });
+  const querySnapshot = await citiesRef
+    .where("date", ">=", Timestamp.fromDate(date.startOf("day").toDate()))
+    .where("date", "<=", Timestamp.fromDate(date.endOf("day").toDate()))
+    .get()
 
-    // return res.data;
-  } catch (errorObj) {
-    throw new Error(`failed to get tasks for ${date}`);
-  }
+  const docsWithData = querySnapshot.docs.map((doc) => {
+    const data = doc.data();
+    data.date = data.date.toDate();
+    return data;
+  });
+  return docsWithData;
+
+  // var docRef = await appDb.collection(`users/${auth.currentUser.uid}/cities`);
+  // citiesRef
+  //   .where("state", "==", "CA")
+  //   .get()
+  //   .then((doc) => {
+  //     if (doc.exists) {
+  //       console.log("Document data:", doc.data());
+  //     } else {
+  //       // doc.data() will be undefined in this case
+  //       console.log("No such document!");
+  //     }
+  //   })
+  //   .catch((error) => {
+  //     console.log("Error getting document:", error);
+  //   });
+
+  // return res.data;
 }
 
 async function createNewTask(task, taskSize, momentjsObj, afterSuccess) {
   try {
-    let refCollection = appDb.collection(
-      `users/${auth.currentUser.uid}/tasklist`
-    );
+    let refCollection = firebase
+      .firestore()
+      .collection(`users/${firebase.auth().currentUser.uid}/tasklist`);
     // TODO Do I need to await these?
     await refCollection.add({
       task,
       taskSize,
-      date: firestore.Timestamp.fromDate(momentjsObj.toDate()),
+      date: Timestamp.fromDate(momentjsObj.toDate()),
       status: false,
-      createdAt: firestore.FieldValue.serverTimestamp(),
+      createdAt: FieldValue.serverTimestamp(),
     });
     afterSuccess();
   } catch (errorObj) {
