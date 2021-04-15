d06aa9627b7b3c862e4cf6821954bb99dd43d259
nathan leezer <62969830+GreenPlow@users.noreply.github.com>
2021-04-15 15:16:55 -0400

    move tasklist css to index css

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+
 import App from "./app/App";
 import reportWebVitals from "./reportWebVitals";
 
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./TaskList.css";
 
 import "./app/config/fire";
 
